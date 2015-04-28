380e8b775c83dbb2ca4003b1861f3c5fc8fd1197
Jerika <al.jerika.reg@gmail.com>
2015-04-28 18:26:49 +0300

    with TouchImageView

--- app/src/main/java/com/my/project/MainActivity.java
+++ app/src/main/java/com/my/project/MainActivity.java
@@ -4,11 +4,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -33,7 +31,7 @@ public class MainActivity extends ActionBarActivity {
     final int REQUEST_CODE_PHOTO = 1;
     final int REQUEST_CODE_GALLERY = 2;
     final String TAG = "myLogs";
-    ImageView ivPhoto;
+    TouchImageView ivPhoto;
     private Matrix matrix = new Matrix();
     private Matrix savedMatrix = new Matrix();
     Matrix m;
@@ -47,11 +45,12 @@ public class MainActivity extends ActionBarActivity {
     private float[] lastEvent = null;
     private float d = 0f;
     private float newRot = 0f;
-    DataBaseHelper helper;
+    static DataBaseHelper helper;
     Bitmap imageToSave;
     FloatingActionsMenu menu;
-    ImageView color;
-    TextView colorName;
+    public static ImageView color;
+    public static TextView colorName;
+    private String selectedImagePath;
 
 
     @Override
@@ -66,10 +65,11 @@ public class MainActivity extends ActionBarActivity {
             e.printStackTrace();
         }
         helper.openDataBase();
-        ivPhoto = (ImageView) findViewById(R.id.ivPhoto);
+        ivPhoto = (TouchImageView) findViewById(R.id.ivPhoto);
         color = (ImageView) findViewById(R.id.imageView);
         colorName = (TextView) findViewById(R.id.textView);
         ivPhoto.setImageResource(R.drawable.color_picker);
+        ivPhoto.setMaxZoom(10);
         menu = (FloatingActionsMenu) findViewById(R.id.main_button);
         if (savedInstanceState != null) {
             imageToSave = savedInstanceState.getParcelable("bitmap");
@@ -120,25 +120,29 @@ public class MainActivity extends ActionBarActivity {
         point.set(x / 2, y / 2);
     }
 
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent intent) {
         switch (requestCode) {
             case REQUEST_CODE_GALLERY:
                 if (resultCode == RESULT_OK) {
-                    ivPhoto.setScaleType(ImageView.ScaleType.MATRIX);
-                    String path = getRealPathFromURI(intent.getData());
-                    matrix.reset();
-                    imageToSave = decodeSampledBitmapFromResource(path, 350, 350);
-                    imageToSave = Bitmap.createScaledBitmap(imageToSave, ivPhoto.getWidth(), ivPhoto.getHeight(), true);
-                    ivPhoto.setImageMatrix(matrix);
-                    ivPhoto.setImageBitmap(imageToSave);
-                    setTouch();
+                    Uri selectedImageUri = intent.getData();
+                    selectedImagePath = getPath(selectedImageUri);
+                    ivPhoto.setImageURI(selectedImageUri);
+                    imageToSave = ((BitmapDrawable)ivPhoto.getDrawable()).getBitmap();
                 }
                 break;
             case REQUEST_CODE_PHOTO:
                 if (resultCode == RESULT_OK) {
-                    ivPhoto.setScaleType(ImageView.ScaleType.MATRIX);
+                    //ivPhoto.setScaleType(ImageView.ScaleType.MATRIX);
                     Boolean isSDPresent = android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
                     File f;
                     if (isSDPresent) {
@@ -153,12 +157,13 @@ public class MainActivity extends ActionBarActivity {
                         }
                     }
                     try {
-                        matrix.reset();
+
                         imageToSave = BitmapFactory.decodeFile(f.getAbsolutePath());
                         imageToSave = Bitmap.createScaledBitmap(imageToSave, ivPhoto.getWidth(), ivPhoto.getHeight(), true);
-                        ivPhoto.setImageMatrix(matrix);
+                        //ivPhoto.setImageMatrix(matrix);
                         ivPhoto.setImageBitmap(imageToSave);
-                        setTouch();
+
+
                        /* String path = android.os.Environment
                                 .getExternalStorageDirectory()
                                 + File.separator
@@ -219,12 +224,21 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    public void FitImageview(Bitmap image){
+        if (image.getWidth() < image.getHeight()){
+
+        }
+
+       /* if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+       else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)*/
+    }
+
     public static Bitmap decodeSampledBitmapFromResource(String path, int reqWidth, int reqHeight) {
         final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
+        /*options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(path, options);
         options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-        options.inJustDecodeBounds = false;
+        options.inJustDecodeBounds = false;*/
         return BitmapFactory.decodeFile(path, options);
     }
 
@@ -253,84 +267,90 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void setTouch() {
-        ivPhoto.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                ImageView view = (ImageView) v;
-                switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                    case MotionEvent.ACTION_DOWN:
-                       // matrix.reset();
-                        savedMatrix.set(matrix);
-                        start.set(event.getX(), event.getY());
-                        mode = DRAG;
-                        lastEvent = null;
-                        break;
-                    case MotionEvent.ACTION_POINTER_DOWN:
-                        oldDist = spacing(event);
-                        if (oldDist > 10f) {
-                            //matrix.reset();
-                            savedMatrix.set(matrix);
-                            midPoint(mid, event);
-                            mode = ZOOM;
-                        }
-                        /*lastEvent = new float[4];
-                        lastEvent[0] = event.getX(0);
-                        lastEvent[1] = event.getX(1);
-                        lastEvent[2] = event.getY(0);
-                        lastEvent[3] = event.getY(1);
-                        //d = rotation(event);*/
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        float eventX = event.getX();
-                        float eventY = event.getY();
-                        float[] eventXY = new float[]{eventX, eventY};
-                        Matrix invertMatrix = new Matrix();
-                        ((ImageView) v).getImageMatrix().invert(invertMatrix);
-                        invertMatrix.mapPoints(eventXY);
-                        int x = (int) eventXY[0];
-                        int y = (int) eventXY[1];
-                        Drawable imgDrawable = view.getDrawable();
-                        Bitmap bitmap = ((BitmapDrawable) imgDrawable).getBitmap();
-                        try {
-                            int touchedRGB = bitmap.getPixel(x, y);
-                            int redValue = Color.red(touchedRGB);
-                            int blueValue = Color.blue(touchedRGB);
-                            int greenValue = Color.green(touchedRGB);
-                            //Toast.makeText(getApplicationContext(), helper.getColor(redValue, greenValue, blueValue),Toast.LENGTH_SHORT).show();
-                            colorName.setText(helper.getColor(redValue, greenValue, blueValue));
-                            color.setBackgroundColor(touchedRGB);
-                        } catch (IllegalArgumentException ignored) {
-                        }
-                        break;
-                    case MotionEvent.ACTION_POINTER_UP:
-                        mode = NONE;
-                        lastEvent = null;
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        if (mode == DRAG) {
-                            matrix.set(savedMatrix);
-                            float dx = event.getX() - start.x;
-                            float dy = event.getY() - start.y;
-                            matrix.postTranslate(dx, dy);
-                        } else if (mode == ZOOM) {
-                            float newDist = spacing(event);
-                            if (newDist > 10f) {
-                                matrix.set(savedMatrix);
-                                float scale = (newDist / oldDist);
-                                matrix.postScale(scale, scale, mid.x, mid.y);
-                            }
-                        }
-                        break;
-                }
-                view.setImageMatrix(matrix);
-                return true;
-            }
-        });
+//        ivPhoto.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                ImageView view = (ImageView) v;
+//                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+//                    case MotionEvent.ACTION_DOWN:
+//                       // matrix.reset();
+//                        savedMatrix.set(matrix);
+//                        start.set(event.getX(), event.getY());
+//                        mode = DRAG;
+//                        lastEvent = null;
+//                        break;
+//                    case MotionEvent.ACTION_POINTER_DOWN:
+//                        oldDist = spacing(event);
+//                        if (oldDist > 10f) {
+//                            //matrix.reset();
+//                            savedMatrix.set(matrix);
+//                            midPoint(mid, event);
+//                            mode = ZOOM;
+//                        }
+//                        /*lastEvent = new float[4];
+//                        lastEvent[0] = event.getX(0);
+//                        lastEvent[1] = event.getX(1);
+//                        lastEvent[2] = event.getY(0);
+//                        lastEvent[3] = event.getY(1);
+//                        //d = rotation(event);*/
+//                        break;
+//                    case MotionEvent.ACTION_UP:
+//                        float eventX = event.getX();
+//                        float eventY = event.getY();
+//                        float[] eventXY = new float[]{eventX, eventY};
+//                        Matrix invertMatrix = new Matrix();
+//                        ((ImageView) v).getImageMatrix().invert(invertMatrix);
+//                        invertMatrix.mapPoints(eventXY);
+//                        int x = (int) eventXY[0];
+//                        int y = (int) eventXY[1];
+//                        Drawable imgDrawable = view.getDrawable();
+//                        Bitmap bitmap = ((BitmapDrawable) imgDrawable).getBitmap();
+//                        try {
+//                            int touchedRGB = bitmap.getPixel(x, y);
+//                            int redValue = Color.red(touchedRGB);
+//                            int blueValue = Color.blue(touchedRGB);
+//                            int greenValue = Color.green(touchedRGB);
+//                            //Toast.makeText(getApplicationContext(), helper.getColor(redValue, greenValue, blueValue),Toast.LENGTH_SHORT).show();
+//                            colorName.setText(helper.getColor(redValue, greenValue, blueValue));
+//                            color.setBackgroundColor(touchedRGB);
+//                        } catch (IllegalArgumentException ignored) {
+//                        }
+//                        break;
+//                    case MotionEvent.ACTION_POINTER_UP:
+//                        mode = NONE;
+//                        lastEvent = null;
+//                        break;
+//                    case MotionEvent.ACTION_MOVE:
+//                        if (mode == DRAG) {
+//                            matrix.set(savedMatrix);
+//                            float dx = event.getX() - start.x;
+//                            float dy = event.getY() - start.y;
+//                            matrix.postTranslate(dx, dy);
+//                        } else if (mode == ZOOM) {
+//                            float newDist = spacing(event);
+//                            if (newDist > 10f) {
+//                                matrix.set(savedMatrix);
+//                                float scale = (newDist / oldDist);
+//                                matrix.postScale(scale, scale, mid.x, mid.y);
+//                            }
+//                        }
+//                        break;
+//                }
+//                view.setImageMatrix(matrix);
+//                return true;
+//            }
+//        });
     }
 
     @Override
     public void onSaveInstanceState(Bundle toSave) {
         super.onSaveInstanceState(toSave);
         toSave.putParcelable("bitmap", imageToSave);
+    }
+
+    public static void setColorName(int redValue, int greenValue,int blueValue, int touchedRGB){
+
+        colorName.setText(helper.getColor(redValue, greenValue, blueValue));
+        color.setBackgroundColor(touchedRGB);
     }
 }
