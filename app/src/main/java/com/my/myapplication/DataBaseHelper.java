58707bc2e3e87d2c387c788711a8fae1ac8d9801
a.kovelskaya <a.kovelskaya@itransition.com>
2019-05-31 11:07:06 +0300

    refactor

---
+++ app/src/main/java/com/my/myapplication/DataBaseHelper.java
@@ -0,0 +1,136 @@
+package com.my.myapplication;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Color;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    //The Android's default system path of your application database.
+    private static String DB_PATH = "/data/data/com.my.myapplication/databases/";
+    private static String DB_NAME = "colors.db";
+    private SQLiteDatabase myDataBase;
+    private final Context myContext;
+
+    /**
+     * Constructor
+     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
+     * @param context
+     */
+    public DataBaseHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+        this.myContext = context;
+    }
+    /**
+     * Creates a empty database on the system and rewrites it with your own database.
+     * */
+    public void createDataBase() throws IOException {
+        boolean dbExist = checkDataBase();
+        if(dbExist){
+            //do nothing - database already exist
+        }else{
+            //By calling this method and empty database will be created into the default system path
+            //of your application so we are gonna be able to overwrite that database with our database.
+            this.getReadableDatabase();
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                throw new Error("Error copying database");
+            }
+        }
+    }
+
+    /**
+     * Check if the database already exist to avoid re-copying the file each time you open the application.
+     * @return true if it exists, false if it doesn't
+     */
+    private boolean checkDataBase(){
+        SQLiteDatabase checkDB = null;
+        try{
+            String myPath = DB_PATH + DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        }catch(SQLiteException e){
+            //database does't exist yet.
+        }
+        if(checkDB != null){
+            checkDB.close();
+        }
+        return checkDB != null ? true : false;
+    }
+
+    /**
+     * Copies your database from your local assets-folder to the just created empty database in the
+     * system folder, from where it can be accessed and handled.
+     * This is done by transfering bytestream.
+     * */
+    private void copyDataBase() throws IOException{
+        //Open your local db as the input stream
+        InputStream myInput = myContext.getAssets().open(DB_NAME);
+        // Path to the just created empty db
+        String outFileName = DB_PATH + DB_NAME;
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(outFileName);
+        //transfer bytes from the inputfile to the outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer))>0){
+            myOutput.write(buffer, 0, length);
+        }
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+
+    }
+
+    public void openDataBase() throws SQLException {
+        //Open the database
+        String myPath = DB_PATH + DB_NAME;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+
+    }
+    @Override
+    public synchronized void close() {
+        if(myDataBase != null)
+            myDataBase.close();
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    public ArrayList<Colors> getColor(int red, int green, int blue){
+        ArrayList<Colors> colorsFromDB = new ArrayList<Colors>();
+        Cursor cursor = myDataBase.rawQuery("SELECT c.*, ( (c.R-" + red + ")*(c.R-" + red + ") " +
+                " +  (c.G-" + green + ")*(c.G-" + green + ")  +  (c.B-" + blue + ")*(c.B-" + blue + ") )" +
+                " AS `distance` FROM colors as c ORDER BY `distance` ASC LIMIT 3", null);
+        cursor.moveToFirst();
+        while (cursor.isAfterLast() == false) {
+            Colors tempColor = new Colors();
+            tempColor.name = cursor.getString(cursor.getColumnIndex("title"));
+            tempColor.color = Color.rgb(cursor.getInt(cursor.getColumnIndex("R")),
+                    cursor.getInt(cursor.getColumnIndex("G")), cursor.getInt(cursor.getColumnIndex("B")));
+            colorsFromDB.add(tempColor);
+            cursor.moveToNext();
+        }
+        return colorsFromDB;
+    }
+
+    public class Colors{
+        String name;
+        int color;
+    }
+}
